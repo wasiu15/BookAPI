8f9ab2471ca8059aa4444abc26243cf970a7661a
wasiu15 <crownprincewadas>
2021-08-22 02:53:29 +0800

    Add project files.

---
+++ BookAPI/Controllers/BooksController.cs
@@ -0,0 +1,41 @@
+﻿using BookAPI.Models;
+using BookAPI.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBookRepository _bookRepository;
+        public BooksController(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Book>> GetBooks()
+        {
+            return await _bookRepository.Get();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> GetBooks(int id)
+        {
+            return await _bookRepository.Get(id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Book>>PostBook([FromBody] Book book)
+        {
+            var newBook = await _bookRepository.Create(book);
+            return CreatedAtAction(nameof(GetBooks), new { id = newBook.Id }, newBook);
+        }
+    }
+}
